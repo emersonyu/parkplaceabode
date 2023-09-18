5893a0ec420e9a82af506d11902ce543190413a9
Emerson Yu <emerson.yu@gmail.com>
2023-09-17 20:55:16 -0700

    feat: add favicon and scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main>
-
       <div className={styles.center}>
         <Image className={styles.logo} src="/ppa_logo_circle_200x200.png" alt="Park Place Abode logo" priority width="100" height="100"/>
       </div>
