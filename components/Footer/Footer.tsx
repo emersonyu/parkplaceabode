df49b6c9aba22c0c00ba6186d7f19966a6efeb76
Emerson Yu <emerson.yu@gmail.com>
2023-09-22 20:53:47 -0700

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,31 +1,32 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { playfairDisplay } from '../../utils/fonts'
 export const Footer = () => {
     return (
-        <footer>
-            <section className="content-container">
-                <div className="logo">
-                    <Link href="/">
-                        <Image
-                            src="/ppa_logo_circle_200x200.png"
-                            alt="Park Place Abode logo"
-                            priority
-                            width="100"
-                            height="100"
-                        />
-                    </Link>
-                </div>
-                <div className="socials">
-                    <ul>
-                        <li>
-                            <a href="https://www.instagram.com/parkplaceabode/">
-                                Instagram
-                            </a>
-                        </li>
-                        <li>Amazon</li>
-                    </ul>
-                </div>
-            </section>
+        <footer className="content-container">
+            <div className="socials" className={playfairDisplay.className}>
+                <ul className="footer-links">
+                    <li>
+                        <a href="https://www.instagram.com/parkplaceabode/">
+                            Instagram
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.amazon.com">Amazon</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="logo">
+                <Link href="/">
+                    <Image
+                        src="/ppa_logo_circle_200x200.png"
+                        alt="Park Place Abode logo"
+                        priority
+                        width="40"
+                        height="40"
+                    />
+                </Link>
+            </div>
         </footer>
     )
 }
